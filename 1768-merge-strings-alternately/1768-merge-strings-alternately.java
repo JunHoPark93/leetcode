d56cc5152809bbfad19051510da151099b65c212
Jun Ho Park <pjh374968@gmail.com>
2023-05-07 22:55:54 +0900

    Time: 0 ms (100.00%), Space: 40.9 MB (60.45%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,22 +1,36 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder sb = new StringBuilder();
-
+        int i = 0, j = 0;
         int w1Len = word1.length();
         int w2Len = word2.length();
-
-        for (int i = 0; i < word1.length(); i++) {
-            if (i >= w2Len) {
-                sb.append(word1.substring(i, word1.length()));
-                break;
+        
+        while (i < w1Len || j < w2Len) {
+            if (i < w1Len) {
+                sb.append(word1.charAt(i++));
             }
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
+            
+            if (j < w2Len) {
+                sb.append(word2.charAt(j++));
+            }
         }
+        
+        // solution above is much more clear
+        // int w1Len = word1.length();
+        // int w2Len = word2.length();
 
-        if (w1Len < w2Len) {
-            sb.append(word2.substring(w1Len, word2.length()));
-        }
+        // for (int i = 0; i < word1.length(); i++) {
+        //     if (i >= w2Len) {
+        //         sb.append(word1.substring(i, word1.length()));
+        //         break;
+        //     }
+        //     sb.append(word1.charAt(i));
+        //     sb.append(word2.charAt(i));
+        // }
+
+        // if (w1Len < w2Len) {
+        //     sb.append(word2.substring(w1Len, word2.length()));
+        // }
 
         return sb.toString();
     }
