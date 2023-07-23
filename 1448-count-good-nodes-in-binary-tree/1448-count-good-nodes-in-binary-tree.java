526e71bf882065961ab036d59fa4c54ec2140593
Jun Ho Park <pjh374968@gmail.com>
2023-07-23 14:55:57 +0900

    Time: 24 ms (5.09%), Space: 55.8 MB (5.13%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,25 +14,47 @@
  * }
  */
 class Solution {
-    private int count = 0;
-
     public int goodNodes(TreeNode root) {
-        find(root, root.val);   
+        int count = 0;
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root, root.val));
+        
+        while (!stack.isEmpty()) {
+            Pair pair = stack.pop();
 
-        return count; 
+            if (pair.node.val >= pair.val) {
+                count++;
+            }
+
+            if (pair.node.left != null) {
+                stack.push(new Pair(pair.node.left, Math.max(pair.node.val, pair.val)));
+            }
+
+if (pair.node.right != null) {
+                stack.push(new Pair(pair.node.right, Math.max(pair.node.val, pair.val)));
+            }
+
+        }
+        return count;
     }
 
-    private void find(TreeNode node, int compare) {
-        if (node.val >= compare) {
-            count++;
-        }
+    class Pair {
+        TreeNode node;
+        int val;
         
-        if (node.left != null) {
-            find(node.left, Math.max(node.val, compare));
-        }
-
-        if (node.right != null) {
-            find(node.right, Math.max(node.val, compare));
+        public Pair(TreeNode node, int val) {
+            this.node = node;
+            this.val = val;
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
